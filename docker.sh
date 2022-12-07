370d54fb4bb8706a5368fc63bb8168c0f0b49320
domibuk <do.buk.m@gmail.com>
2022-12-07 17:02:10 +0100

    Add autoscaling and load balancer

--- docker.sh
+++ docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -ex
+
 sudo yum update -y
 sudo yum install -y docker
 sudo systemctl start docker
-docker run -p 8080:80 nginx
+docker run -p 80:80 nginx
--- main.tf
+++ main.tf
@@ -11,46 +11,103 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-###Security Groups###
-resource "aws_security_group" "db-app-sg" {
-  name   = "db-app-sg"
+#Security Groups
+resource "aws_security_group" "demoproject_instance" {
+  name   = "demoproject-instance-sg"
   vpc_id = "vpc-371de54e"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.demoproject_lb.id]
+  }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = []
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = []
-  }
-  tags = {
-    Name : "db-app-sg"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-###EC2###
-resource "aws_instance" "db-app-instance" {
-  ami           = "ami-01cae1550c0adea9c"
-  instance_type = "t2.micro"
+resource "aws_security_group" "demoproject_lb" {
+  name   = "demoproject-lb-sg"
+  vpc_id = "vpc-371de54e"
 
-  vpc_security_group_ids = [aws_security_group.db-app-sg.id]
-
-  user_data = file("docker.sh")
-  
-  tags = {
-    Name = "DbDockerInstance"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#EC2
+resource "aws_launch_configuration" "demoproject" {
+  name_prefix                 = "EC2-asg-"
+  image_id                    = "ami-01cae1550c0adea9c"
+  instance_type               = "t2.micro"
+  user_data                   = file("docker.sh")
+  security_groups             = [aws_security_group.demoproject_instance.id]
+  associate_public_ip_address = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#Autoscaling
+resource "aws_autoscaling_group" "demoproject" {
+  min_size             = 1
+  max_size             = 2
+  desired_capacity     = 2
+  launch_configuration = aws_launch_configuration.demoproject.name
+  vpc_zone_identifier  = ["subnet-24a5a06c", "subnet-d96257bf"]
+}
+
+#Application load balancer
+resource "aws_lb" "demoproject" {
+  name               = "demoproject-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.demoproject_lb.id]
+  subnets            = ["subnet-24a5a06c", "subnet-d96257bf"]
+}
+
+resource "aws_lb_listener" "demoproject" {
+  load_balancer_arn = aws_lb.demoproject.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.demoproject.arn
+  }
+}
+
+resource "aws_lb_target_group" "demoproject" {
+  name     = "demoproject-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-371de54e"
+}
+
+resource "aws_autoscaling_attachment" "demoproject" {
+  autoscaling_group_name = aws_autoscaling_group.demoproject.id
+  lb_target_group_arn    = aws_lb_target_group.demoproject.arn
 }
